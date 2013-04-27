209a0b9cf72d7d85b908a6b8d32a3403d62fc2a6
juffel <julian.dobmann@googlemail.com>
2013-04-27 17:25:26 +0200

    terminal.write() funzt wohl

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 var terminal = new Terminal();
+
+terminal.write("Test, test!");
+terminal.write("Laeuft immernoch?");
+terminal.write("\n");
+terminal.write("Schoen, schoen!");
--- terminal.js
+++ terminal.js
@@ -1,27 +1,65 @@
 function Terminal() {
-    document.writeln("<canvas id='screen' width='" + window.innerWidth + "' height='" + window.innerHeight + "'></canvas>")
+    var width = window.innerWidth - 15;
+    var height = window.innerHeight - 10;
+    document.writeln("<canvas id='screen' width='" + width + "' height='" + height + "'></canvas>")
     unloadScrollbars();
+
+    // settings
+    var fontType = "bold 20px TlwgTypewriter";
+    var textSize = 20;
+    var textColor = "#00FF00";
+    var leftMargin = 5;
+    var backgroundColor = "black";
+    var inputRowHeight = textSize*2;
+
+    var screen;
+    var ctx;
+    var content = []; // array of lines of text
 
     _init();
 
-    var content = []; // array of lines of text
-
     // cursor must blink!
 
-    function _drawContent() {
+    function write(line) {
+        content.push(line);
+        _drawContent();
+    }
+
+    function writeln(line) {
+        write(line + "\n");
+    }
+
+    function setInputHandler() {
+        // TODO
+    }
+
+    function clear() {
+        content = [];
+        _clear();
+    }
+
+    function _clear() {
         ctx.clearRect(0, 0, screen.width, screen.height);
+        ctx.fillStyle = backgroundColor;
+        ctx.fillRect(0, 0, screen.width, screen.height);
+    }
+
+    function _drawContent() {
+        _clear();
+        for(var i = content.length; i > 0 && screen.height - inputRowHeight - textSize*(content.length - i) >= 0 ; i--) {
+            ctx.fillStyle = textColor;
+            ctx.font = fontType;
+            ctx.fillText(content[i-1], leftMargin, screen.height - inputRowHeight - (i*textSize));
+        }
     }
 
     function _init() {
         // blacken screen
-        var screen = document.getElementById("screen");
-        var ctx = screen.getContext("2d");
-        ctx.fillStyle = "black";
+        screen = document.getElementById("screen");
+        ctx = screen.getContext("2d");
+        ctx.fillStyle = backgroundColor;
         ctx.fillRect(0, 0, screen.width, screen.height);
-        // set textstyle
-        ctx.fillStyle = "#00FF00";
-        ctx.font = "bold 20px TlwgTypewriter";
-        ctx.fillText("WELCOME!", 5, 20);
+        write("WELCOME!");
     }
     
     return {
