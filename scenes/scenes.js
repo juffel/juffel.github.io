848fc2a4830041343f076c90a7974549a0e61b21
juffel <julian.dobmann@googlemail.com>
2013-04-28 20:52:08 +0200

    some test scenes added

---
+++ scenes/scenes.js
@@ -0,0 +1,20 @@
+var start = new Scene("start", "Eventually its gonna be a textadventure!");
+start.setDescription("Begin? Or Credits?");
+start.addOption("begin", ["begin", "start"], "begin");
+start.addOption("credits", ["credits", "credits"], "credits");
+
+var credits = new Scene("credits", ["CREDITS"]);
+credits.setDescription("+++ juffel +++");
+credits.addOption("begin", ["begin", "start"], "begin");
+
+var sc = new Scene("begin", "IN THE BEGINNING");
+sc.setDescription("The Story begins. You are sitting on a backless chair and feel the intense pain of not knowing.");
+sc.addOption("stand up", ["Stand", "Up", "Get"], "standing");
+
+var sc03i = new Scene("standing", "UP IN THE AIR");
+sc03i.setDescription("From up here everything looks different. You focus on some details in the painting on the bulldozer in front of you.");
+
+var end = new Scene("end", "THIS IS THE END");
+end.setDescription("And then they lived happily ever after.");
+
+var scenes = [start, credits, sc, sc03i, end];
