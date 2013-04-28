17e087a2c048650ea4c9cb5744fc8438acfaef72
juffel <julian.dobmann@googlemail.com>
2013-04-29 00:47:11 +0200

    SceneExample

---
+++ scenes/scenesForm.js
@@ -0,0 +1,15 @@
+----------------------
+HOW TO DESCRIBE SCENES
+----------------------
+<ID> / <Title>\n
+<Description>\n
+<option_0>: tags\n
+...
+<option_n>: tags\n
+----------------------
+Maybe / Maybe - Maybe not
+Your inner self is not quite sure about this. Whatever you are going to do, nothing will have an impact on where this story goes.
+Desperation: desperation, desperate, done
+Author: write, direction, create, creative
+Storytelling: story, narrate, narration, novel, game
+----------------------
